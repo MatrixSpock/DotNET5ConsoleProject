bc50b6467520023d3fcb2d3dfc4a40c505aecef8
JoeIbarra <91492108+JoeIbarra@users.noreply.github.com>
2022-04-01 17:28:33 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace _NET5ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(JsonConvert.SerializeObject(args));
+        }
+    }
+}
